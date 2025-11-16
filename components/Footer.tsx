dd64a1d61067d573f37123a3ecb3bc89dbb34687
payjuper <yj1son@naver.com>
2025-11-16 00:55:45 -0600

    Switch to react web app

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-  return (
-    <footer className="py-10 text-center text-sm text-white/60">
-      <p>© {new Date().getFullYear()} Young · Built with Next.js & Tailwind.</p>
-    </footer>
-  )
-}
